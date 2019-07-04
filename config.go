0cf1a3f6b6af225ce2a698d499b9dde082cb9f0c
nicolas.dorier <nicolas.dorier@gmail.com>
2019-07-04 19:16:25 +0900

    Allow unsafe authentication on regtest

--- config.go
+++ config.go
@@ -1067,7 +1067,8 @@ func loadConfig() (*config, error) {
 	if err != nil {
 		return nil, err
 	}
-
+    
+    if activeNetParams.Params.Name != "regtest" {
 	// For each of the RPC listeners (REST+gRPC), we'll ensure that users
 	// have specified a safe combo for authentication. If not, we'll bail
 	// out with an error.
@@ -1089,6 +1090,7 @@ func loadConfig() (*config, error) {
 			return nil, err
 		}
 	}
+    }
 
 	// Remove the listening addresses specified if listening is disabled.
 	if cfg.DisableListen {

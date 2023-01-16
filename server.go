9a8ae30a5c240ba2d1fd95df99fc5367c77c9f84
djkazic <kevincai96@gmail.com>
2023-01-16 14:40:24 -0500

    Do not override lncfg defined value for StrictZombiePruning

--- server.go
+++ server.go
@@ -1107,9 +1107,6 @@ func newServer(ctx context.Context, cfg *Config, listenAddrs []net.Addr,
 
 	s.controlTower = routing.NewControlTower(dbs.PaymentsDB)
 
-	strictPruning := cfg.Bitcoin.Node == "neutrino" ||
-		cfg.Routing.StrictZombiePruning
-
 	s.graphBuilder, err = graph.NewBuilder(&graph.Config{
 		SelfNode:            nodePubKey,
 		Graph:               dbs.GraphDB,
@@ -1120,7 +1117,7 @@ func newServer(ctx context.Context, cfg *Config, listenAddrs []net.Addr,
 		GraphPruneInterval:  time.Hour,
 		FirstTimePruneDelay: graph.DefaultFirstTimePruneDelay,
 		AssumeChannelValid:  cfg.Routing.AssumeChannelValid,
-		StrictZombiePruning: strictPruning,
+		StrictZombiePruning: cfg.Routing.StrictZombiePruning,
 		IsAlias:             aliasmgr.IsAlias,
 	})
 	if err != nil {

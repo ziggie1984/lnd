5fcffbfeefaff4e32386c80281ab856e4d1b3a4b
Evan Kaloudis <zeusln@tutanota.com>
2024-12-18 15:23:07 -0500

    Impl addinvoice minroutinghints
    Co-authored-by: djkazic <kevincai96@gmail.com>

--- lnrpc/invoicesrpc/addinvoice_test.go
+++ lnrpc/invoicesrpc/addinvoice_test.go
@@ -469,7 +469,9 @@ func TestShouldIncludeChannel(t *testing.T) {
 
 var sufficientHintsTestCases = []struct {
 	name          string
+	nCurrentHints int
 	nHintsLeft    int
+	nMinHopHints  int
 	currentAmount lnwire.MilliSatoshi
 	targetAmount  lnwire.MilliSatoshi
 	done          bool
@@ -483,6 +485,23 @@ var sufficientHintsTestCases = []struct {
 	name:       "enough hints",
 	nHintsLeft: 0,
 	done:       true,
+}, {
+	name:          "not enough hints (minimum)",
+	nCurrentHints: 1,
+	nHintsLeft:    0,
+	nMinHopHints:  2,
+	done:          false,
+}, {
+	name:          "enough hints (minimum)",
+	nCurrentHints: 2,
+	nHintsLeft:    0,
+	nMinHopHints:  2,
+	done:          true,
+}, {
+	name:          "current hints",
+	nCurrentHints: 20,
+	nHintsLeft:    0,
+	done:          true,
 }, {
 	name:          "enough bandwidth",
 	nHintsLeft:    1,
@@ -505,7 +524,10 @@ func TestSufficientHints(t *testing.T) {
 			t.Parallel()
 
 			enoughHints := sufficientHints(
-				tc.nHintsLeft, tc.currentAmount,
+				tc.nCurrentHints,
+				tc.nHintsLeft,
+				tc.nMinHopHints,
+				tc.currentAmount,
 				tc.targetAmount,
 			)
 			require.Equal(t, tc.done, enoughHints)
--- rpcserver.go
+++ rpcserver.go
@@ -6454,6 +6454,7 @@ func (r *rpcServer) AddInvoice(ctx context.Context,
 		RouteHints:      routeHints,
 		Amp:             invoice.IsAmp,
 		BlindedPathCfg:  blindedPathCfg,
+		MinHopHints:     invoice.MinHopHints,
 	}
 
 	if invoice.RPreimage != nil {
